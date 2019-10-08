4d7d00d8a2d4b6d51426ec838b420e233523f1a4
David <davidjesus1284@gmail.com>
2019-10-08 13:25:40 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://davidjesus1284:Da12vid84@cluster0-misls.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
