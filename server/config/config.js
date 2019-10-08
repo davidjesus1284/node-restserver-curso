1c8a9ea560710c745d8ffc35c2c12f7fc6ce2cfb
David <davidjesus1284@gmail.com>
2019-10-08 11:23:53 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 //=============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=============================
+// Entorno
+//=============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+// Base de Datos
+//=============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://davidjesus1284:Da12vid84@cluster0-misls.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
